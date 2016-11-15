b3e9fa577186a194a10c5232b0001a4f42258568
max sami <skull037037@gmail.com>
2016-11-15 15:17:19 -0800

    Collision fix
    apple collision should be fixed, apples varraibles were not being
    updated

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-var PlayerX = 175;
-var PlayerY = 20;
-var apple1X=Number(document.getElementById("apple1").getAttribute("x"));
-var apple1y=Number(document.getElementById("apple1").getAttribute("y"));
-var apple2X=Number(document.getElementById("apple2").getAttribute("x"));
-var apple2y=Number(document.getElementById("apple2").getAttribute("y"));
+var PlayerX = Number(document.getElementById("Player").getAttribute("x"));
+var PlayerY = Number(document.getElementById("Player").getAttribute("y"));
+
 
 document.addEventListener("keydown", function(e) {
+  var apple1X=Number(document.getElementById("apple1").getAttribute("x"));
+  var apple1y=Number(document.getElementById("apple1").getAttribute("y"));
+  var apple2X=Number(document.getElementById("apple2").getAttribute("x"));
+  var apple2y=Number(document.getElementById("apple2").getAttribute("y"));
   if(e.keyCode == 37 || e.keyCode == 65){
     PlayerX -=10;
  document.getElementById("Player").setAttribute("x", PlayerX)
@@ -26,10 +27,10 @@ document.addEventListener("keydown", function(e) {
  document.getElementById("Player").setAttribute("y", PlayerY)
   }
 //checks players location against apple's location
-  if(PlayerX >= apple1X-15 && PlayerX <= apple1X+15 && PlayerY >= apple1y-15 && PlayerY <= apple1y+15 ){
+  if(PlayerX >= apple1X-10 && PlayerX <= apple1X+10 && PlayerY >= apple1y-10 && PlayerY <= apple1y+10 ){
     document.getElementById("apple1").setAttribute("x",600);
   }
-  else if(PlayerX >= apple2X-15 && PlayerX <= apple2X+15 && PlayerY >= apple2y-15 && PlayerY <= apple2y+15 ){
+  else if(PlayerX >= apple2X-10 && PlayerX <= apple2X+10 && PlayerY >= apple2y-10 && PlayerY <= apple2y+10 ){
     document.getElementById("apple2").setAttribute("x",600);
   }
 });
