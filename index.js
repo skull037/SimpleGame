53f1b396c7b2d8160a67e587236f7acb96270901
max sami <skull037037@gmail.com>
2016-11-15 08:16:42 -0800

    Part 3
    Added an apple for food which falls endlessly and decreased player
    movment speed

--- index.js
+++ index.js
@@ -2,22 +2,22 @@ var PlayerX = 175;
 var PlayerY = 20;
 document.addEventListener("keydown", function(e) {
   if(e.keyCode == 37 || e.keyCode == 65){
-    PlayerX -=15;
+    PlayerX -=10;
  document.getElementById("Player").setAttribute("x", PlayerX)
 
   }
   else if(e.keyCode == 39|| e.keyCode == 68){
-    PlayerX +=15;
+    PlayerX +=10;
  document.getElementById("Player").setAttribute("x", PlayerX)
 
   }
   if(e.keyCode == 40 || e.keyCode == 83){
-    PlayerY +=15;
+    PlayerY +=10;
  document.getElementById("Player").setAttribute("y", PlayerY)
 
   }
   else if(e.keyCode == 38|| e.keyCode == 87){
-    PlayerY -=15;
+    PlayerY -=10;
  document.getElementById("Player").setAttribute("y", PlayerY)
 
   }
