ad17734b88105a017535118a4cafeebf008c6743
max sami <skull037037@gmail.com>
2016-11-15 09:09:09 -0800

    Part 4
    apple detection sort of works, apples get moved off screen for now

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 var PlayerX = 175;
 var PlayerY = 20;
+var apple1X=Number(document.getElementById("apple1").getAttribute("x"));
+var apple1y=Number(document.getElementById("apple1").getAttribute("y"));
+var apple2X=Number(document.getElementById("apple2").getAttribute("x"));
+var apple2y=Number(document.getElementById("apple2").getAttribute("y"));
+
 document.addEventListener("keydown", function(e) {
   if(e.keyCode == 37 || e.keyCode == 65){
     PlayerX -=10;
@@ -19,6 +24,12 @@ document.addEventListener("keydown", function(e) {
   else if(e.keyCode == 38|| e.keyCode == 87){
     PlayerY -=10;
  document.getElementById("Player").setAttribute("y", PlayerY)
-
+  }
+//checks players location against apple's location
+  if(PlayerX >= apple1X-15 && PlayerX <= apple1X+15 && PlayerY >= apple1y-15 && PlayerY <= apple1y+15 ){
+    document.getElementById("apple1").setAttribute("x",600);
+  }
+  else if(PlayerX >= apple2X-15 && PlayerX <= apple2X+15 && PlayerY >= apple2y-15 && PlayerY <= apple2y+15 ){
+    document.getElementById("apple2").setAttribute("x",600);
   }
 });
